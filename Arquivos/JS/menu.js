bc30ace7535ed87c6f586352f18bec99b85d860a
AndreVeiga <soutomachado1@gmail.com>
2019-06-27 17:39:49 -0300

    Teste

---
+++ Arquivos/JS/menu.js
@@ -0,0 +1,11 @@
+function w3_open() {
+    var x = document.getElementById("mySidebar");
+    x.style.width = "100%";
+    x.style.fontSize = "40px";
+    x.style.paddingTop = "10%";
+    x.style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}  
